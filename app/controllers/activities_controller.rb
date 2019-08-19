e5d0f77fe555e8300dab9864234bd9bf223d5cb2
Jason Dixon <jasondixonmail@gmail.com>
2019-08-18 20:41:25 -0700

    Created controller for activity

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,11 @@
+class ActivitiesController < ApplicationController
+    def create
+        @trip = Trip.find(params[:trip_id])
+
+        if @trip.activities.create(activity_params)
+            redirect_to @trip, notice: 'Activity was successfully created.'
+        else
+            redirect_to @trip, alert: 'Error creating activity.'
+        end
+    end
+end
