6b7bed0de626129baab297c422928857918e54f1
Jason Dixon <jasondixonmail@gmail.com>
2019-08-18 21:16:56 -0700

    wip to add form for adding activities. getting error when trying to save

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
     def create
         @trip = Trip.find(params[:trip_id])
 
-        if @trip.activities.create(activity_params)
+        if @trip.activities.create(params)
             redirect_to @trip, notice: 'Activity was successfully created.'
         else
             redirect_to @trip, alert: 'Error creating activity.'
