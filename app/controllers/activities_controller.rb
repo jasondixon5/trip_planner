d817e3471ecf61c08c834c26e1e0b4378496dbca
Jason Dixon <jasondixonmail@gmail.com>
2019-08-18 21:39:10 -0700

    Activities are saving but where field entry is storing as name and name is not storing.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,10 +2,14 @@ class ActivitiesController < ApplicationController
     def create
         @trip = Trip.find(params[:trip_id])
 
-        if @trip.activities.create(params)
+        if @trip.activities.create(activity_params)
             redirect_to @trip, notice: 'Activity was successfully created.'
         else
             redirect_to @trip, alert: 'Error creating activity.'
         end
     end
+    
+    def activity_params
+        params.require(:activity).permit(:name, :where)
+      end
 end
