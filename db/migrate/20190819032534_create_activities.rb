ad4d7b3e3034c554331bd2c83c3fc88cea81fe49
Jason Dixon <jasondixonmail@gmail.com>
2019-08-18 20:26:14 -0700

    added model Activity

---
+++ db/migrate/20190819032534_create_activities.rb
@@ -0,0 +1,11 @@
+class CreateActivities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.string :where
+      t.references :trip, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
