67d6fb02df7238244646ec2b04670977b955302a
Jason Dixon <jasondixonmail@gmail.com>
2019-08-18 20:11:37 -0700

    added start_date col to trips table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_030555) do
+ActiveRecord::Schema.define(version: 2019_08_19_031109) do
 
   create_table "trips", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
   end
 
 end
